b14231493924a9e3761f0fa3ae00a39afabbc1fc
crccheck <c@crccheck.com>
2014-07-14 18:45:32 -0500

    add initial sketch of models

---
+++ wjordpress/models.py
@@ -0,0 +1,82 @@
+"""
+
+field names are taken to match the json output (except always lowercase).
+
+`max_length`s of 255 means I just picked an arbitrary high number and does not
+reflect some real schema limit.
+"""
+from django.db import models
+
+
+class WPSite(models.Model):
+    url = models.URLField(unique=True)
+    name = models.CharField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name or self.url
+
+    def get_absolute_url(self):
+        return self.url
+
+
+class WPAuthor(models.Model):
+    id = models.PositiveIntegerField(unique=True)
+    slug = models.SlugField(max_length=255)
+    url = models.URLField(null=True, blank=True,
+        help_text='The url the author has set as their homepage.')
+    name = models.CharField(max_length=255)
+    avatar = models.URLField(null=True, blank=True)
+    # first_name
+    # last_name
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return self.url
+
+
+class WPPost(models.Model):
+    id = models.PositiveIntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)  # choices? `publish`
+    type = models.CharField(max_length=255)  # choices? `post`
+    content = models.TextField(null=True, blank=True)
+    link = models.URLField()
+    date = models.DateTimeField()
+    modified = models.DateTimeField()
+    slug = models.SlugField(max_length=255)
+    excerpt = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return self.link
+
+
+class WPTag(models.Model):
+    id = models.PositiveIntegerField(unique=True)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    link = models.URLField()
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return self.link
+
+
+class WPCategory(models.Model):
+    id = models.PositiveIntegerField(unique=True)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    link = models.URLField()
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return self.link
