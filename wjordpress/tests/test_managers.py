86b91326c04d7ce56c757abced815b7c96776221
crccheck <c@crccheck.com>
2014-07-15 11:57:18 -0500

    document how many queries it took

--- wjordpress/tests/test_managers.py
+++ wjordpress/tests/test_managers.py
@@ -16,7 +16,8 @@ class WPPostManagerTest(TestCase):
         self.assertIsInstance(WPPost.objects, WPPostManager)
         data = json.load(open(os.path.join(BASE_DIR, 'support', 'posts.json')))
         site = WPSiteFactory()
-        WPPost.objects.get_or_create_from_resource_list(site, data)
+        with self.assertNumQueries(140):
+            WPPost.objects.get_or_create_from_resource_list(site, data)
         # assert 8 posts were created
         self.assertEqual(WPPost.objects.count(), 8)
         # assert 1 user was created
