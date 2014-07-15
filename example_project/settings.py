bd018740392d33104eaf95e8e285fe8dfe231b69
crccheck <c@crccheck.com>
2014-07-14 22:03:14 -0500

    update settings from boilerplate

--- example_project/settings.py
+++ example_project/settings.py
@@ -1,13 +1,9 @@
 # Django settings for example_project project.
 import os
+BASE_DIR = os.path.dirname(__file__)
 
 import dj_database_url
 from project_runpy import env
-
-
-def project_dir(*paths):
-    base = os.path.realpath(os.path.dirname(__file__))
-    return os.path.join(base, *paths)
 
 
 # default to DEBUG=True
@@ -21,7 +17,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {'default': dj_database_url.config(default='sqlite:///' +
-    project_dir('example_project.db'))}
+    os.path.join(BASE_DIR, 'example_project.db'))}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -52,7 +48,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = project_dir('..', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -63,7 +59,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = project_dir('static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -74,7 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    project_dir('static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -82,7 +78,6 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -92,7 +87,6 @@ SECRET_KEY = 'lolimasekrit'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -114,7 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    project_dir('templates'),
+    os.path.join(BASE_DIR, 'templates'),
 )
 
 INSTALLED_APPS = [
@@ -133,52 +127,40 @@ INSTALLED_APPS = [
     'django_extensions',
 ]
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    # Log everything
     'root': {
-        'level': 'DEBUG',
+        'level': os.environ.get('LOGGING_LEVEL', 'WARNING'),
         'handlers': ['console'],
     },
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
-        }
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+        'readable_sql': {
+            '()': 'project_runpy.ReadableSqlFilter',
+        },
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
         'console': {
             'level': 'DEBUG',
             'class': 'project_runpy.ColorizingStreamHandler',
         },
     },
     'loggers': {
-        # Keep logs from filling up with sql
         'django.db.backends': {
-            'level': 'ERROR',
+            'level': 'DEBUG' if env.get('SQL') else 'INFO',
             'handlers': ['console'],
-            'propagate': True,
+            'filters': ['require_debug_true', 'readable_sql'],
+            'propagate': False,
         },
-        'django.request': {
-            'handlers': ['mail_admins'],
+        'factory': {
             'level': 'ERROR',
-            'propagate': True,
-        },
-        # Silence nose a bit
-        'nose': {
-            'level': 'WARNING',
-            'handlers': ['console'],
-            'propagate': True,
+            'propagate': False,
         },
     }
 }
