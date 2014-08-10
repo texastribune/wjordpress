3a4deda5c7bb42aded55f01f1285d206fe59d7f5
crccheck <c@crccheck.com>
2014-08-10 18:47:31 -0500

    get coverage up a little

--- wjordpress/factories.py
+++ wjordpress/factories.py
@@ -4,6 +4,9 @@ from django.utils.timezone import now
 
 from .models import (
     WPSite,
+    WPUser,
+    WPTag,
+    WPCategory,
     WPPost,
 )
 
@@ -14,7 +17,34 @@ __all__ = ['WPSiteFactory', 'WPPostFactory']
 class WPSiteFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = WPSite
 
-    url = factory.Sequence(lambda i: 'http://{}.sux'.format(i))
+    url = factory.Sequence(lambda i: 'http://{}.site'.format(i))
+
+
+class WPUserFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = WPUser
+
+    wp = factory.SubFactory(WPSiteFactory)
+    id = factory.Sequence(int)
+    synced_at = factory.LazyAttribute(lambda __: now())
+    url = factory.Sequence(lambda i: 'http://{}.user'.format(i))
+
+
+class WPTagFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = WPTag
+
+    wp = factory.SubFactory(WPSiteFactory)
+    id = factory.Sequence(int)
+    synced_at = factory.LazyAttribute(lambda __: now())
+    link = factory.Sequence(lambda i: 'http://{}.link'.format(i))
+
+
+class WPCategoryFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = WPCategory
+
+    wp = factory.SubFactory(WPSiteFactory)
+    id = factory.Sequence(int)
+    synced_at = factory.LazyAttribute(lambda __: now())
+    link = factory.Sequence(lambda i: 'http://{}.link'.format(i))
 
 
 class WPPostFactory(factory.django.DjangoModelFactory):
@@ -25,3 +55,4 @@ class WPPostFactory(factory.django.DjangoModelFactory):
     synced_at = factory.LazyAttribute(lambda __: now())
     date = factory.LazyAttribute(lambda __: now())
     modified = factory.LazyAttribute(lambda __: now())
+    link = factory.Sequence(lambda i: 'http://{}.post'.format(i))
--- wjordpress/tests/test_models.py
+++ wjordpress/tests/test_models.py
@@ -2,7 +2,10 @@ import json
 import os
 
 from . import WPTestCase
-from ..factories import WPSiteFactory, WPPostFactory
+from ..factories import (
+    WPSiteFactory, WPUserFactory, WPTagFactory, WPCategoryFactory,
+    WPPostFactory,
+)
 
 
 BASE_DIR = os.path.dirname(__file__)
@@ -21,12 +24,53 @@ class WPObjectModelTest(WPTestCase):
 
 
 class WPSiteTest(WPTestCase):
+    def setUp(self):
+        self.site = WPSiteFactory()
+        super(WPTestCase, self).setUp()
+
     def test_get_absolute_url_works(self):
-        site = WPSiteFactory()
-        self.assertTrue(site.get_absolute_url())
+        self.assertTrue(self.site.get_absolute_url())
 
     def test_hook_url_works(self):
-        site = WPSiteFactory()
-        self.assertTrue(site.hook_url)
+        self.assertTrue(self.site.hook_url)
         # like traditional get_absolute_url, is actually a path
-        self.assertTrue(site.hook_url.startswith('/'))
+        self.assertTrue(self.site.hook_url.startswith('/'))
+
+    def test_save_from_resource_works(self):
+        # not a very good test, should use real data
+        mock_data = {
+            'name': 'hello',
+            'description': 'world',
+        }
+        self.site.save_from_resource(mock_data)
+        self.assertEqual(self.site.name, 'hello')
+        self.assertEqual(self.site.description, 'world')
+
+
+class WPUserTest(WPTestCase):
+    def test_get_absolute_url_works(self):
+        user = WPUserFactory()
+        self.assertTrue(user.get_absolute_url())
+
+
+class WPTagTest(WPTestCase):
+    def test_get_absolute_url_works(self):
+        tag = WPTagFactory()
+        self.assertTrue(tag.get_absolute_url())
+
+
+class WPCategoryTest(WPTestCase):
+    def test_get_absolute_url_works(self):
+        category = WPCategoryFactory()
+        self.assertTrue(category.get_absolute_url())
+
+
+class WPPostTest(WPTestCase):
+    def test_get_absolute_url_works(self):
+        post = WPPostFactory()
+        self.assertTrue(post.get_absolute_url())
+
+    def test_images_works(self):
+        # assert if post has no meta, it has no images
+        post = WPPostFactory()
+        self.assertIsNone(post.images)
