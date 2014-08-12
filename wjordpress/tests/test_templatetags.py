ff6de16f81037722ec1786b6a92724d20faa145a
crccheck <c@crccheck.com>
2014-08-11 22:04:16 -0500

    add content to the widget template

--- wjordpress/tests/test_templatetags.py
+++ wjordpress/tests/test_templatetags.py
@@ -1,7 +1,7 @@
 from django.template import Context, Template
 
 from . import WPTestCase
-from ..factories import WPSiteFactory
+from ..factories import WPSiteFactory, WPPostFactory
 
 
 class WidgetTestCase(WPTestCase):
@@ -12,7 +12,32 @@ class WidgetTestCase(WPTestCase):
             t.render(Context())
 
     def test_wjidget_renders(self):
-        WPSiteFactory(name='foo')
+        num_posts = 3  # XXX this is less than the default limit
+        wp_site = WPSiteFactory(name='foo')
+        for i in range(num_posts):
+            WPPostFactory(
+                wp=wp_site,
+                type='post',
+                status='publish',
+                title='{}'.format(i),
+                excerpt='{}'.format(i),
+            )
         t = Template('{% load wjidget from wjordpress %}{% wjidget "foo" %}')
         output = t.render(Context())
-        self.assertIn('wjordpress-widget', output)
+        self.assertIn('wjidget-container', output)
+        self.assertEqual(output.count('<article>'), num_posts)
+
+    def test_wjidget_can_be_limited(self):
+        wp_site = WPSiteFactory(name='foo')
+        for i in range(11):
+            WPPostFactory(
+                wp=wp_site,
+                type='post',
+                status='publish',
+                title='{}'.format(i),
+                excerpt='{}'.format(i),
+            )
+        t = Template('{% load wjidget from wjordpress %}'
+            '{% wjidget "foo" limit=5 %}')
+        output = t.render(Context())
+        self.assertEqual(output.count('<article>'), 5)
